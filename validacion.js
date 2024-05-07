dd73aa06c2f2db0f67383895c739f05d83b38b78
santiago5231 <67811067+santiago5231@users.noreply.github.com>
2024-05-07 12:04:14 -0500

    Update validacion.js

--- validacion.js
+++ validacion.js
@@ -1,26 +1,21 @@
-//Haz tú validación en javascript acá
 
+jQuery(document).ready(function(){
 
-const generatePDFButton = document.getElementById('generatePDFButton');
+  $('#generatePDFButton').click(function()
+  {
+ 
+  window.print();
 
-generatePDFButton.addEventListener('click', generatePDF);
+  });
 
-function generatePDF() {
-    
-    var html = document.body.innerHTML;
+//$('#generatePDFButton').click(function(){
+  //html2canvas(document.querySelector('#Portafolio')).then((canvas)=>  {
+    //let base64image = canvas.toDataURL('image/png');
 
-    
-    var options = {
-      filename: 'mi-portafolio.pdf', 
-      pageFormat: 'A4', 
-      margin: [20, 20, 20, 20], 
-      orientation: 'portrait', 
-    };
+    //let pdf= new jsPDF('p','px',[1600,1131]);
+    //pdf.addImage(base64image, 'PNG', 15, 15, 1110, 381);
+    //pdf.save('Descarga-curriculum-PDF');
 
-    // Convert HTML to PDF and trigger download
-    html2pdf().from()
-    .save();
-    
-}
+  });
 
 
